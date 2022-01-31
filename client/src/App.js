29d78c128966fffa730ba9b9da9b9afdbef6a846
ikke09 <development@fam-bechtluft.de>
2022-02-01 00:26:14 +0100

    Cleanup client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -17,23 +15,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>
           API-Version: {version.version}
         </p>
-      </header>
     </div>
   );
 }
