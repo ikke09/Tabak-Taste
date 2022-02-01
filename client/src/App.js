250dcbe3a3e9be8e45274f25b52da4b1b812a685
ikke09 <development@fam-bechtluft.de>
2022-02-01 13:36:57 +0100

    [InWork] Simple app basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import { Box, TextField, InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import './App.css';
+import Tobacco from './components/Tobacco';
 
 function App() {
 
-  const [version, setVersion] = useState({});
+  const [api, setApi] = useState('/api/tobaccos?name=');
+  const [query, setQuery] = useState("");
+  const [tobaccos, setTobaccos] = useState([]);
 
   useEffect(() => {
-    
-    fetch('/api')
+    console.log(query);
+    setApi(api.replace(new RegExp('name=.*', 'gi'), `name=${query}`));
+    console.log(api);
+    fetch(api)
       .then(res => res.json())
-      .then(data => setVersion(data));
-  }, []);
+      .then(data => setTobaccos(data));
+  }, [query]);
   
-
   return (
-    <div className="App">
-        <p>
-          API-Version: {version.version}
-        </p>
-    </div>
+    <Box
+    sx={{
+      width: '80vw',
+      height: '90vh',
+      alignItems: 'center',
+      backgroundColor: 'primary.dark',
+      '&:hover': {
+        backgroundColor: 'primary.main',
+        opacity: [0.9, 0.8, 0.7],
+      },
+    }} >
+    <TextField
+        fullWidth
+        id="input-query"
+        label="Suche nach dem gewünschten Tabak"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        variant="outlined"
+        value={query}
+        onChange={changeEvent => setQuery(changeEvent.target.value)}
+      />
+      {
+        tobaccos && tobaccos.length > 1
+        ? tobaccos.map(tobacco => <Tobacco model={tobacco} />)
+        : <p>No Data loaded yet!</p>
+      }
+  </Box>
   );
 }
 
---
+++ client/src/components/Tobacco.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Tobacco = (props) => {
+    return (
+        <div>
+            {props.model.name}
+        </div>
+    )
+};
+
+export default Tobacco;
