a2c87287d9b686443a89bac72a697ea7ee1fe54c
ikke09 <development@fam-bechtluft.de>
2022-02-04 12:03:49 +0100

    Improve Search styling

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,30 +1,62 @@
 import React from 'react';
-import { TextField, InputAdornment } from '@mui/material';
+import { TextField, InputAdornment, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import CancelIcon from '@mui/icons-material/Cancel';
 import { styled } from '@mui/system';
 
 const StyledInput = styled(TextField)(({ theme }) => ({
     boxShadow: `-0.1em -0.1em 0.6em 0.1em ${theme.palette.primary.highlight}, 0.1em 0.1em 0.6em 0.1em ${theme.palette.primary.highlight}`,
-    color: 'black',
     border: `0.15em solid black`,
     borderRadius: '0.75em',
-    width: '25%'
+    width: '40%',
+    minWidth: '300px',
+    "& .MuiOutlinedInput-root": {
+        "& fieldset": { 
+            border: "none"
+        },
+        "&.Mui-focused fieldset": {
+            border: "none"
+        },
+    },
+    "& input": {
+        fontSize: 'large',
+        color: `${theme.palette.primary.card}`,
+    }
+}));
+
+const ColoredSearchIcon = styled(SearchIcon)(({ theme }) => ({
+    color: `${theme.palette.primary.card}`,
+}));
+
+const ColoredCancelIcon = styled(CancelIcon)(({ theme }) => ({
+    color: `${theme.palette.primary.card}`,
 }));
 
 export default function Search(props) {
   return (
     <StyledInput
         id="input-query"
+        variant="outlined"
+        margin="normal"
+        autoComplete="off"
+        value={props.data}
+        onChange={props.handleChange}
         InputProps={{
             startAdornment: (
                 <InputAdornment position="start">
-                    <SearchIcon />
+                    <ColoredSearchIcon />
                 </InputAdornment>
             ),
+            endAdornment: props.data && (
+                <IconButton
+                  aria-label="Eingabe löschen"
+                  onClick={() => props.handleChange({target: {value: ""}})}
+                >
+                    <ColoredCancelIcon />
+                </IconButton>
+              )
         }}
-        variant="outlined"
-        value={props.data}
-        onChange={props.handleChange}
+        
     />
   );
 }
