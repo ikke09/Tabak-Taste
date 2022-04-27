bc304741c28221985206ef961276ca97deef42b5
ikke09 <development@fam-bechtluft.de>
2022-04-27 19:15:02 +0200

    Better readable path.join to builded index.html

--- api.js
+++ api.js
@@ -44,7 +44,7 @@ app.use(helmet());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build' , 'index.html'));
 })
 
 app.listen(port, () => {
