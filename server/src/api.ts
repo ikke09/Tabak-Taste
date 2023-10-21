944fc30d1ebe01396192d4e04df8f064eb789f72
ikke09 <development@fam-bechtluft.de>
2023-10-21 12:40:40 +0200

    Introduce config and serve swagger only in dev mode

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+import Config from "./config";
 import express, { Express } from "express";
 import path from "path";
 import helmet from "helmet";
@@ -8,30 +8,25 @@ import { PrismaClient } from "@prisma/client";
 const app = express();
 const prisma = new PrismaClient();
 
-const swaggerUI = require("swagger-ui-express");
-// Generated with // https://editor.swagger.io/
-const openAPI = require("./openapi.json");
-const openAPIOptions = require("./openapi-options.json");
-
-const port = process.env.PORT || process.env.API_PORT;
-const dbOptions = {
-  user: process.env.DB_USER,
-  pass: process.env.DB_PW,
-};
-
-mongoose.connect(process.env.DB_CONNECTION, dbOptions, (error) => {
-  if (error) console.error(error);
-  else console.debug("DB connected!");
-});
-
 app.use(cors());
 app.use(helmet());
-app.use(express.static(path.join(__dirname, "client/build")));
-app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(openAPI, openAPIOptions));
+app.use(express.static(path.join(__dirname, "../../client/build")));
+
+if (Config.NODE_ENV === "development") {
+  const swaggerUI = require("swagger-ui-express");
+  // Generated with // https://editor.swagger.io/
+  const openAPI = require("../resources/openapi.json");
+  const openAPIOptions = require("../resources/openapi-options.json");
+  app.use(
+    "/api/docs",
+    swaggerUI.serve,
+    swaggerUI.setup(openAPI, openAPIOptions)
+  );
+}
 
 app.get("/api", (req, res) => {
   res.json({
-    version: process.env.API_VERSION,
+    version: Config.API_VERSION,
   });
 });
 
@@ -41,35 +36,37 @@ app.get("/api/tobaccos", (req, res) => {
     return;
   }
 
-  const findOptions = { $or: [] };
-  const searchParameter = req.query.search;
-  const nameOption = { name: { $regex: `${searchParameter}`, $options: "i" } };
-  findOptions.$or.push(nameOption);
-  const producerNameOption = {
-    "producer.name": { $regex: `${searchParameter}`, $options: "i" },
-  };
-  findOptions.$or.push(producerNameOption);
-  const tasteOption = {
-    tastes: { $elemMatch: { $regex: `${searchParameter}`, $options: "i" } },
-  };
-  findOptions.$or.push(tasteOption);
+  // const findOptions = { $or: [] };
+  // const searchParameter = req.query.search;
+  // const nameOption = { name: { $regex: `${searchParameter}`, $options: "i" } };
+  // findOptions.$or.push(nameOption);
+  // const producerNameOption = {
+  //   "producer.name": { $regex: `${searchParameter}`, $options: "i" },
+  // };
+  // findOptions.$or.push(producerNameOption);
+  // const tasteOption = {
+  //   tastes: { $elemMatch: { $regex: `${searchParameter}`, $options: "i" } },
+  // };
+  // findOptions.$or.push(tasteOption);
 
-  Tobacco.find(findOptions, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.json([]);
-      return;
-    }
+  // Tobacco.find(findOptions, (err, data) => {
+  //   if (err) {
+  //     console.error(err);
+  //     res.json([]);
+  //     return;
+  //   }
 
-    const tobaccos = data.map((tobaccoFromDb) => new TobaccoDto(tobaccoFromDb));
-    res.json(tobaccos);
-  });
+  //   const tobaccos = data.map((tobaccoFromDb) => new TobaccoDto(tobaccoFromDb));
+  //   res.json(tobaccos);
+  // });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "..", "..", "client", "build", "index.html")
+  );
 });
 
-app.listen(port, () => {
-  console.debug(`API listening on Port ${port}`);
+app.listen(Config.API_PORT, () => {
+  console.debug(`API listening on Port ${Config.API_PORT}`);
 });
