4692a1066a137d637e7d65e709bf64441ac05d2f
ikke09 <development@fam-bechtluft.de>
2023-11-01 10:26:41 +0100

    Setup proxy and env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.PROXY,
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        headers: {
+          Connection: "keep-alive",
+        },
+        configure: (proxy) => {
+          proxy.on("error", (err) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+    },
+  },
+});
