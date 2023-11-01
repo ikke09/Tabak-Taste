ae9c915ba3d2885e06d54d01576ae16615b02216
ikke09 <development@fam-bechtluft.de>
2023-11-01 21:56:37 +0100

    use env in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,35 +1,44 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.PROXY,
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        headers: {
-          Connection: "keep-alive",
-        },
-        configure: (proxy) => {
-          proxy.on("error", (err) => {
-            console.log("proxy error", err);
-          });
-          proxy.on("proxyReq", (proxyReq, req) => {
-            console.log("Sending Request to the Target:", req.method, req.url);
-          });
-          proxy.on("proxyRes", (proxyRes, req) => {
-            console.log(
-              "Received Response from the Target:",
-              proxyRes.statusCode,
-              req.url
-            );
-          });
+export default defineConfig(({ mode }: UserConfig) => {
+  const env = loadEnv(mode, process.cwd(), "VITE");
+  console.debug(env);
+  const config: UserConfig = {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_PROXY,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          headers: {
+            Connection: "keep-alive",
+          },
+          configure: (proxy) => {
+            proxy.on("error", (err) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req) => {
+              console.log(
+                "Sending Request to the Target:",
+                req.method,
+                req.url
+              );
+            });
+            proxy.on("proxyRes", (proxyRes, req) => {
+              console.log(
+                "Received Response from the Target:",
+                proxyRes.statusCode,
+                req.url
+              );
+            });
+          },
         },
       },
     },
-  },
+  };
+  return config;
 });
