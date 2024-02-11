db95a612405a39e5a171a9359912b90d21ebd454
ikke09 <development@fam-bechtluft.de>
2024-02-11 21:59:02 +0100

    [WIP] More docker

--- apps/tabak-taste-api/src/config.ts
+++ apps/tabak-taste-api/src/config.ts
@@ -1,18 +1,10 @@
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config';
+import { z } from 'zod';
 
 const configSchema = z.object({
-  NODE_ENV: z.union([z.literal("development"), z.literal("production")]),
-  API_VERSION: z.string().optional().default("0.0.1"),
+  NODE_ENV: z.union([z.literal('development'), z.literal('production')]),
+  API_VERSION: z.string().optional().default('0.0.1'),
   API_PORT: z.coerce.number().optional().default(3000),
-  DB_HOST: z.string(),
-  DB_PORT: z.coerce.number().optional().default(27017),
-  DB_USER: z.string(),
-  DB_PASSWORD: z.string(),
-  DB_NAME: z.string(),
-  DB_UI_USER: z.string(),
-  DB_UI_PASSWORD: z.string(),
-  DATABASE_URL: z.string().startsWith("postgresql://"),
 });
 
 const Config = configSchema.parse(process.env);
